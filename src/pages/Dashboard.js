3e74d9afb76b861eed6aa04eb1930d46a5e6abe5
ykolambe <ykolambe123@gmail.com>
2020-10-15 15:04:31 +0400

    dsdsd

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -181,41 +181,10 @@ class Dashboard extends React.Component {
 				</div>
 			</Col>
 			</Row>
+			
 			<Row>
             <Col style={{ display: "flex" }}>
-              <div className={styles.block2_1} xs={12} sm={12}>
-                <div className={styles.buttonsBlock}>
-                  <PlainButton
-                    className={styles.btnCss}
-                    title="Top 5 contest"
-                    onKeyPress={(e) => e.key === "Enter" && this.didLogin()}
-                    onClick={this.didLogin}
-                  />
-                  <PlainButton
-                    className={styles.btnCss}
-                    title="Top 5 contest"
-                    onKeyPress={(e) => e.key === "Enter" && this.didLogin()}
-                    onClick={this.didLogin}
-                  />
-                  <PlainButton
-                    className={styles.btnCss}
-                    title="Top 5 contest"
-                    onKeyPress={(e) => e.key === "Enter" && this.didLogin()}
-                    onClick={this.didLogin}
-                  />
-                </div>
-                <div className={styles.addBlock}></div>
-              </div>
-              <div className={styles.block2_2} xs={12} sm={12}>
-                {CONTEST_LIST.map((obj, index) => (
-                  <div className={styles.contestList}>
-                    <div className={styles.contestListItem}>
-                      <span>{obj.name}</span>
-                      <span>{obj.votes}</span>
-                    </div>
-                  </div>
-                ))}
-              </div>
+              <div className={styles.addBlock}></div>
             </Col>
           </Row>
           <Row>
